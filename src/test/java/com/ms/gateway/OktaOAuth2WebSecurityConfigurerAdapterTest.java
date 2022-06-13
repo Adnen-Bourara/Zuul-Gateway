4680fb4ad735fc0ac18d5288aef276b617e691bc
adnen <adnen.bourara@esprit.tn>
2022-06-14 00:36:03 +0100

    Initial commit

---
+++ src/test/java/com/ms/gateway/OktaOAuth2WebSecurityConfigurerAdapterTest.java
@@ -0,0 +1,14 @@
+package com.ms.gateway;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class OktaOAuth2WebSecurityConfigurerAdapterTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
